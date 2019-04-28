23ec1afe2de96ddfdf5548b1f20c160efd175f4f
Bodi44 <shtohrinets@ucu.edu.ua>
2019-04-28 18:16:25 +0300

    Updates

---
+++ precache-manifest.2f2e5e01c4e153a92854274f8f7e02fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a9605fc240c05af25fd63f3864d18f",
+    "url": "/souvenir-shop-app/index.html"
+  },
+  {
+    "revision": "eb43ec60726608b4aba5",
+    "url": "/souvenir-shop-app/static/css/main.c59eeb54.chunk.css"
+  },
+  {
+    "revision": "096210af439533cacbfb",
+    "url": "/souvenir-shop-app/static/js/2.15001d47.chunk.js"
+  },
+  {
+    "revision": "eb43ec60726608b4aba5",
+    "url": "/souvenir-shop-app/static/js/main.3bfceb63.chunk.js"
+  },
+  {
+    "revision": "b3c6cb280f9e46a61371",
+    "url": "/souvenir-shop-app/static/js/runtime~main.03833fbf.js"
+  }
+]);
