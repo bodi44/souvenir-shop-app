ee50157463b1c2acd98739ce9eaf4ff3c004377b
Bodi44 <shtohrinets@ucu.edu.ua>
2019-04-30 00:04:27 +0300

    Updates

---
+++ precache-manifest.5e38fada33933a3fa0d8dcf085f34c70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31dd72dece17ed9ebce04951d4c05796",
+    "url": "/souvenir-shop-app/index.html"
+  },
+  {
+    "revision": "506e64ae8aa46d13a7f4",
+    "url": "/souvenir-shop-app/static/css/main.57f0c802.chunk.css"
+  },
+  {
+    "revision": "100ac079d23e5dc04529",
+    "url": "/souvenir-shop-app/static/js/2.7e0f964e.chunk.js"
+  },
+  {
+    "revision": "506e64ae8aa46d13a7f4",
+    "url": "/souvenir-shop-app/static/js/main.cfa2206e.chunk.js"
+  },
+  {
+    "revision": "b3c6cb280f9e46a61371",
+    "url": "/souvenir-shop-app/static/js/runtime~main.03833fbf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/souvenir-shop-app/precache-manifest.7af709601ce3b9f5f0f8a8dfca9cb280.js"
+  "/souvenir-shop-app/precache-manifest.5e38fada33933a3fa0d8dcf085f34c70.js"
 );
 
 self.addEventListener('message', (event) => {
